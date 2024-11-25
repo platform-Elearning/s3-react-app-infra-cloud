42885ac48d895ab4fcddaf7bfc42bea4670afe97
julimax <julimax951@gmail.com>
2024-11-25 14:51:35 -0300

    going to production!

--- aws-s3/uploadweb.tf
+++ aws-s3/uploadweb.tf
@@ -1,19 +1,39 @@
-
 resource "aws_s3_bucket_object" "site_files" {
   for_each = fileset("${path.module}/web", "**/*")
 
-  bucket = aws_s3_bucket.bucket_web.id
-  key    = replace(each.value, "^web/", "")
-  source = "${path.module}/web/${each.value}"
-  content_type = lookup({
+  bucket        = aws_s3_bucket.bucket_web.id
+  key           = replace(each.value, "^web/", "")
+  source        = "${path.module}/web/${each.value}"
+  content_type  = lookup({
     "tml" : "text/html",
     "css"  : "text/css",
     "jpg"  : "image/jpeg",
-    "peg" : "image/jpeg",
+    "peg"  : "image/jpeg",
     "png"  : "image/png",
     "gif"  : "image/gif",
     "svg"  : "image/svg+xml",
-    ".js"   : "application/javascript",
+    ".js"  : "application/javascript",
   }, lower(substr(basename(each.value), -3, 3)), "application/octet-stream")
+
+  etag = filemd5("${path.module}/web/${each.value}")
 }
 
+resource "aws_s3_bucket_object" "site_files_dev" {
+  for_each = fileset("${path.module}/web-dev", "**/*")
+
+  bucket        = aws_s3_bucket.bucket_web_dev.id
+  key           = replace(each.value, "^web/", "")
+  source        = "${path.module}/web-dev/${each.value}"
+  content_type  = lookup({
+    "tml" : "text/html",
+    "css"  : "text/css",
+    "jpg"  : "image/jpeg",
+    "peg"  : "image/jpeg",
+    "png"  : "image/png",
+    "gif"  : "image/gif",
+    "svg"  : "image/svg+xml",
+    ".js"  : "application/javascript",
+  }, lower(substr(basename(each.value), -3, 3)), "application/octet-stream")
+
+  etag = filemd5("${path.module}/web-dev/${each.value}")
+}
